444b09961001446ade093fa9c030c04da3147385
Salvador Ahumada <s_ahumada@ar3d.net>
2019-08-14 11:47:38 -0500

    minor frontend fix

--- frontend/views/components/contractsSegment.vue
+++ frontend/views/components/contractsSegment.vue
@@ -6,10 +6,10 @@
       </div>
       <div class="row row-full" :class="{ animate: isActive }" style="margin:0 auto;">
 
-        <div class="col-sm-0 col-md-1 col-lg-1 col-xl-3">
+        <div class="col-sm-0 col-md-1 col-lg-1 col-xl-4">
         </div>
 
-        <div class="col-sm-12 col-md-10 col-lg-10 col-xl-6" style="display:flex;align-items:center;">
+        <div class="col-sm-12 col-md-10 col-lg-10 col-xl-4" style="display:flex;align-items:center;">
           <div class="card">
             <div class="card-body">
               <h4 class="page-title">Contract Application</h4>
@@ -244,7 +244,7 @@
           </div>
         </div>
 
-        <div class="col-sm-0 col-md-1 col-lg-1 col-xl-3">
+        <div class="col-sm-0 col-md-1 col-lg-1 col-xl-4">
         </div>
 
       </div>
