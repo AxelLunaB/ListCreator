883fcdad60f9ba9ee31fd3d86b5f47ecad72bed8
bbarajasar3d <b_barajas@ar3d.net>
2019-09-03 12:11:33 -0500

    Added getContractsByPaidRef method to retrieve contracts that are paid

--- frontend/store/modules/contracts/getters.js
+++ frontend/store/modules/contracts/getters.js
@@ -3,9 +3,11 @@ const query = (state) => state.query;
 const isListGetter = (state) => state.isList;
 const filterValue = (state) => state.filterValue;
 const specialSort = (state) => state.specialFilter;
+const contractsByPaidRef = (state) => state.contractsByPaidRef;
 
 export default {
   contracts,
+  contractsByPaidRef,
   query,
   isListGetter,
   filterValue,
